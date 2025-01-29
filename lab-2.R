4d34af9e00ac3ca1aa23c83d76f411b75050b0a4
Michael Boateng <mboateng@colgate.edu>
2025-01-28 19:31:04 -0500

    Completed Task 1 (actually)

--- lab-2.R
+++ lab-2.R
@@ -1,9 +1,9 @@
 #TASK 1
 #command to list all directories in Music
 directories <- list.dirs("Music")
-#count the number of forward sla
+#count the number of forward slashes
 ct <- str_count(directories, "/")
-#subsetting album directories from all subdirectories
+#sub-setting album directories from all sub-directories
 albums <- directories[which(ct == 2)]
 
 songs = c()
@@ -13,12 +13,12 @@ for(album in albums){
   num <- str_count(files, ".wav")
   songs <- files[which(num >= 1)]
   for (song in songs){
-    location <- paste('"',list.dirs(album), list.files(song), '"', sep="")
-    track_name <- str_sub(song, end = str_length(song)-4)S
-    track_name <- paste(unlist(str_split(track_name, "-")), collapse = " ")
+    location <- paste('"',list.dirs(album), song, '"', sep="")
+    track_name <- str_sub(song, end = str_length(song)-4)
+    track_name <- paste(track_name, sep = "-")
     split_album <- str_split(album, "/", simplify = T)
     json_filename <- paste(split_album[2], "-", split_album[3], "-", track_name, ".json", sep = "")
-    cmd <- paste("streaming-extractor_music.exe", location, json_filename)
+    cmd <- paste("streaming_extractor_music.exe", location, paste('"', json_filename, '"', sep = ""))
     code.to.process = c(code.to.process, cmd)
   }
 }
@@ -27,6 +27,6 @@ writeLines(code.to.process, "batfile.txt")
 
 # some fixes
 # streaming-extractor_music.exe OfficeStuff Zip                                      OfficeStuff-Backpack-OfficeStuff Zip.json
-# streaming-extractor_music.exe "Music/OfficeStuff/Backpack/01-OfficeStuff-Zip.wav" "OfficeStuff-Backpack-Zip.json"
+# streaming_extractor_music.exe "Music/OfficeStuff/Backpack/01-OfficeStuff-Zip.wav" "OfficeStuff-Backpack-Zip.json"
 
 
